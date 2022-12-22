95f12007ce50dad24dce271f1200e366993604c9
Svea Apps <apps@svea.com>
2022-12-22 10:53:22 +0100

    Svea Wallet v. 3.1.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SveaWallet", 
-            url: "https://maven.svea.com/repository/internal/com/svea/ios/SveaWallet/SveaWallet-3.0.0.xcframework.zip", 
-            checksum: "08da28c7fdcd840fe6b4fc9277504b3dbe6183fc69c4c838efa50d22072b7b32")
+            url: "https://maven.svea.com/repository/internal/com/svea/ios/SveaWallet/SveaWallet-3.1.0.xcframework.zip", 
+            checksum: "fe41a4d25459abb17ca0943242e6232f6771baefada48c8c5c16e34713aef309")
     ]
 )
